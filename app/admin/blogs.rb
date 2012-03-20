6baf75c36e4b7cbb7adc7c65049f22d59bd39c50
railosmith <railosmith@garage.(none)>
2012-03-20 14:41:12 +0530

    added editor to blogs in admin

--- app/admin/blogs.rb
+++ app/admin/blogs.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Blog do
-  
+ #custom form
+form :html => { :multipart => true } do |f|
+f.inputs "Blogs" do
+ f.input :title
+ f.input :body, :input_html => { :class => "ckeditor" }
+end
+ f.buttons
+end  
 end
